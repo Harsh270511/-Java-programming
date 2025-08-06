c6970d1187541b3b4a2ee0abacac53cda18133f0
Harsh270511 <hm0093173@gmail.com>
2025-08-07 00:02:22 +0530

    Add files via upload

---
+++ conversionCasting.java
@@ -0,0 +1,12 @@
+package unit3;
+
+public class conversionCasting {
+
+  public static void main(String[] args) {
+    float num = 5;
+    System.out.println(num);
+
+    int number = (int) 12.9f;// it helps to convert float number into integer and it is explicit way
+    System.out.println(number);
+  }
+}
