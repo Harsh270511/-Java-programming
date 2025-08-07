a2610ab8fb0e4e4c270234e4d626e5c93b60f476
Harsh270511 <hm0093173@gmail.com>
2025-08-08 05:26:23 +0530

    Add files via upload

---
+++ escape1.java
@@ -0,0 +1,19 @@
+package unit3;
+
+public class escape1 {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+
+    System.out.println("Hello \"KgCoding\"");// here \ will helps to print with double inverted comma
+
+    System.out.println("hello\tKgCoding"); //\t gives a tab space or use 4 times spacebar key
+
+    System.out.println("hello\bKgCoding");// will insert backspace
+
+    System.out.println("Hello \nKgCoding");// after \n new line 
+
+    System.out.println("Hello \'kGCoding\'"); // will print with single cotes
+
+    System.out.println("hello \\kgcoding\\");// will print backslash
+  }
+}
